5d34a358d5f531437813e277097732e3a2ba8fc1
gustavo-henrique-pereira-lima <gustavohenriquelive12@gmail.com>
2023-11-10 11:29:13 -0300

    Validação de usuario incompleta

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  /* public loginForm!: FormGroup */
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router: Router) { }
+
+  public loginForm: FormGroup = this.formBuilder.group({
+    email: [''],
+    senha: ['']
+  })
+
+  login(){
+    this.http.get<any>("http://localhost:3000/usuarios")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.senha === this.loginForm.value.senha
+      });
+        if(user){
+          alert("Login efetuado com sucesso!");
+          this.loginForm.reset();
+          this.router.navigate(['dashboard'])
+        }else{
+          alert("Usuario nao encontrado!")
+        }
+    },_err=>{
+      alert("Something Wrong!")
+    })
+  }
 
 }
